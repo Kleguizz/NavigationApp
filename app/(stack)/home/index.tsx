81030a7daf9cba0105cf9daeb019dc408cfa1f5e
kevin <kd.leguizamon@gmail.com>
2025-04-03 21:18:33 -0300

    first commit

---
+++ app/(stack)/home/index.tsx
@@ -0,0 +1,54 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link, router } from "expo-router";
+import CustomButton from "@/components/shared/CustomButton";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View className="px-10 mt-5">
+        <CustomButton
+          className="mb-2"
+          color="primary"
+          onPress={() => router.push("/products")}
+        >
+          Productos
+        </CustomButton>
+
+        <CustomButton
+          className="mb-2"
+          color="secondary"
+          onPress={() => router.push("/profile")}
+        >
+          Profile
+        </CustomButton>
+
+        <CustomButton
+          className="mb-2"
+          color="tertiary"
+          onPress={() => router.push("/settings")}
+        >
+          Settings
+        </CustomButton>
+
+        <Link href="/products" asChild>
+          <CustomButton className="mb-10" color="primary" variant="text-only">
+            Productos
+          </CustomButton>
+        </Link>
+        {/* <Link className="mb-5" href="./products">
+          Productos
+        </Link>
+        <Link className="mb-5" href="./profile">
+          Perfil
+        </Link>
+        <Link className="mb-5" href="./settings">
+          Ajustes
+        </Link> */}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import { View, Text } from "react-native";
+import "./global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link, Redirect } from "expo-router";
+
+const App = () => {
+  return <Redirect href="/home" />;
+
+  // <SafeAreaView>
+  //   <View className="mt-6 mx-2.5">
+  //     <Text className="text-5xl " style={{ fontFamily: "WorkSans-Black" }}>
+  //       Hola mundo
+  //     </Text>
+
+  //     <Text className="text-4xl font-work-black text-primary">
+  //       Hola mundo
+  //     </Text>
+  //     <Text className="text-3xl font-work-light text-secondary">
+  //       Hola mundo
+  //     </Text>
+  //     <Text className="text-2xl font-work-medium text-secondary-100">
+  //       Hola mundo
+  //     </Text>
+  //     <Text className="text-2xl font-work-medium text-tertiary">
+  //       Hola mundo
+  //     </Text>
+
+  //     <Link href="/products">Productos</Link>
+  //   </View>
+  // </SafeAreaView>
+};
+
+export default App;
